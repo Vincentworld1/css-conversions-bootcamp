6cf44780f769d225bde08c197a9d1932c60cd52a
root <root@LAPTOP-9LUOLFTB.localdomain>
2021-07-29 09:40:31 +0800

    base Hex to RGB

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('hello world');
+
+const hexCode = process.argv[2];
+console.log(hexCode)
+
+
+function hexToRgb(hex) {
+  const result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+  return result ? {
+    r: parseInt(result[1], 16),
+    g: parseInt(result[2], 16),
+    b: parseInt(result[3], 16),
+  } : null;
+}
+
+
+const rgbValue = hexToRgb(hexCode);
+console.log(rgbValue);
+
